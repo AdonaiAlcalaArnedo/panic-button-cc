7619a469c21ef858450fec6bbe7cd933336a368a
AdonaiAlcalaArnedo <adonaialcalaarnedo@gmail.com>
2026-04-24 18:56:42 -0500

    feat:FIX GRAFICA  modulo BI con estadisticas, ranking operadores y exportar Excel

--- src/pages/Estadisticas.jsx
+++ src/pages/Estadisticas.jsx
@@ -396,17 +396,15 @@ export default function Estadisticas() {
             <ResponsiveContainer width="100%" height={220}>
               <PieChart>
                 <Pie
-                  data={porTipo}
-                  dataKey="value"
-                  nameKey="name"
-                  cx="50%"
-                  cy="50%"
-                  outerRadius={80}
-                  label={({ name, percent }) =>
-                    `${name} ${(percent * 100).toFixed(0)}%`
-                  }
-                  labelLine={false}
-                >
+                    data={porTipo}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={75}
+                    label={({ percent }) => `${(percent * 100).toFixed(0)}%`}
+                    labelLine={false}
+                    >
                   {porTipo.map((entry, i) => (
                     <Cell key={i} fill={entry.color} />
                   ))}
