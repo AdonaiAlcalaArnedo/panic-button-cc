3bdbb7525b032eaa608fd9df0ae54bb99b038793
AdonaiAlcalaArnedo <adonaialcalaarnedo@gmail.com>
2026-04-24 21:22:41 -0500

    feat:FIX GRAFICA  modulo BI con estadisticas, ranking operadores y exportar Excel

--- src/pages/Estadisticas.jsx
+++ src/pages/Estadisticas.jsx
@@ -401,7 +401,7 @@ export default function Estadisticas() {
                     nameKey="name"
                     cx="50%"
                     cy="55%"
-                    outerRadius={80}
+                    outerRadius={75}
                     label={({ percent }) => `${(percent * 100).toFixed(0)}%`}
                     labelLine={false}
                     >
